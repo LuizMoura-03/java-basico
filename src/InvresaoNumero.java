baaa890c694b4ba34b1e9975d9b59ec1058b9ae9
LuizFernando <barbosa.luiz@zup.com.br>
2024-12-09 20:48:27 -0300

    Completed the class InversaoNumero

--- src/InvresaoNumero.java
+++ src/InvresaoNumero.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class InvresaoNumero {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero: ");
+        int numero = scanner.nextInt();
+
+        int invertido = 0;
+
+        while (numero != 0) {
+            invertido = invertido * 10;   // pega o ultimo digito.
+            numero /= 10;  //Remove o ultimo digito
+
+        }
+
+        System.out.println("Numero invrertido: " + invertido);
+
+        scanner.close();
 
     }
 }
